caf38f2bfdb0bdb34403b277fed0889181102829
Dmitrandir <dim9107@mail.ru>
2022-08-13 16:41:26 +0300

    New ViewController added

---
+++ LoginApp/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  LoginApp
+//
+//  Created by Дмитрий Солопов on 13.08.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
